2547e78b2d8946c7c134dbf1b3d3181af7527ba2
Robin Dumont-Chaponet <src@dumontchapo.net>
2019-08-02 00:03:27 +0200

    Update RoutingException.php

--- src/RoutingException.php
+++ src/RoutingException.php
@@ -4,4 +4,16 @@ namespace Transitive\Routing;
 
 class RoutingException extends \UnexpectedValueException
 {
+    private $queryURL;
+
+    public function __construct(string $message = '', int $code = 0, string $queryURL = null) {
+        $this->queryURL = $queryURL;
+
+        parent::__construct($message, $code);
+    }
+
+    public function getQueryURL(): ?string
+    {
+        return $this->queryURL;
+    }
 }
